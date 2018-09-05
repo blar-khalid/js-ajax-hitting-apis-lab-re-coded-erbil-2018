7ff431f01270f48d358de72b68be808cd0e5d0e9
blar khalid <blarharki@gmail.com>
2018-09-05 18:39:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const url = "https://github.com/blar-khalid"
 
 // repos
-function datRepositories() {
+function getRepositories() {
   const name = document.getElementById("username").value
   const xhr = new XMLHttpRequest()
-  xhr.addEventListener("load", displayRepositories)
+  xhr.addEventListener("load", R)
   xhr.open("GET",url+ "/users/" + name + "/repos")
   xhr.send();
 }
-function show() {
+function showR() {
   const repos = JSON.parse(this.responseText)
   const repoList = "<ul>" + repos.map(repo => {
     const username = 'username="' + repo.owner.login + '"'
@@ -17,7 +17,7 @@ function show() {
           <li>
             <h2>${repo.name}</h2>
             <a href="${repo.html_url}">${repo.html_url}</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${repoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
             <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
           </li>`
           )
@@ -30,11 +30,11 @@ function show() {
 function getCommits(el) {
   const repoName = el.dataset.repository
   const xhr = new XMLHttpRequest()
-  xhr.addEventListener("load", displayCommits)
+  xhr.addEventListener("load", showC)
   xhr.open("GET", url+ "/repos/" + el.dataset.username + "/" + repoName + "/commits")
   xhr.send()
 }
-function show() {
+function showC() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
@@ -46,11 +46,11 @@ function getBranches(el) {
   const repoName = el.dataset.repository
   
   const xhr = new XMLHttpRequest()
-  xhr.addEventListener("load", displayBranches)
+  xhr.addEventListener("load", showb)
   xhr.open("GET", url + "/repos/" + el.dataset.username + "/" + repoName + "/branches")
   xhr.send()
 }
-function show() {
+function showb() {
   const branches = JSON.parse(this.responseText)
   const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = branchesList
